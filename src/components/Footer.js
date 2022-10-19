cf77c622276c563190692499f5d3e95553d7e252
Mrfoxc121 <foxc121@yahoo.co.uk>
2022-10-19 21:28:47 +0100

    built home

---
+++ src/components/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Logo from "../img/foxesBlog.png";
+
+
+const Footer = () => {
+  return (
+    <footer>
+      <img src={Logo} alt="" />
+      <span>Made by<b> James Jenson Web Solutions &copy; 2022</b></span>
+    </footer>
+  )
+}
+
+export default Footer
