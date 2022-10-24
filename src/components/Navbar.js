79a1d193755f8c39f7c558e68eba5c57ca7622fd
Mrfoxc121 <foxc121@yahoo.co.uk>
2022-10-24 10:29:03 +0100

    first commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,10 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AuthContext } from "../context/authContext";
 import Logo from "../img/foxesBlog.png";
 
 const Navbar = () => {
+  const {currentUser, logout} = useContext(AuthContext)
   return (
     <div className="navbar">
       <div className="container">
@@ -29,8 +31,14 @@ const Navbar = () => {
             <h6>FOOD</h6>
           </Link>
           <div className="profile">
-            <span>John</span>
-            <span>Logout</span>
+            <span>{currentUser?.username}</span>
+            {currentUser ? (
+            <span onClick={logout}>Logout</span>
+          ) : (
+            <Link className="link" to="/login">
+              Login
+            </Link>
+          )}
           </div>
 
           <span className="write">
